a50abf0ec842955bf0cc8a44f167f29a094a541d
Salim DELLALI <dellali.salim@gmail.com>
2022-02-15 23:51:51 +0100

    handled edge cases where the user inputs a wrong pokemon name, and an empty pokemon name

--- src/handlers.ts
+++ src/handlers.ts
@@ -8,6 +8,11 @@ export async function getPokemonByName(
   const https = require("https");
   const name: string = request.params["name"];
 
+  if (name === null || name.trim() === "") {
+    sendError(reply, "Please set a valid pokemon name", 400);
+    return;
+  }
+
   https
     .get(`https://pokeapi.co/api/v2/pokemon/${name}`, (result) => {
       let data = [];
@@ -17,12 +22,19 @@ export async function getPokemonByName(
       });
 
       result.on("end", () => {
+        if (result.statusCode === 404) {
+          sendError(reply, `Pokemon with name ${name} does not exist`, 404);
+          return;
+        }
+
         const pokemon: PokemonWithStats = buildPokemon(data);
         reply.send(pokemon);
       });
     })
     .on("error", (err) => {
       console.error("Error: ", err.message);
+      sendError(reply, "Something went wrong ¯\_(ツ)_/¯", 500);
+      return;
     });
 }
 
@@ -37,4 +49,9 @@ const buildPokemon = (data) => {
 
 const getStatsAverage = (stats) => {
   return stats.reduce((acc, cur) => acc + cur.base_stat, 0) / stats.length;
+}
+
+const sendError = (reply, errorMessage, statusCode) => {
+  reply.statusCode = statusCode;
+  reply.send({ errorMessage });
 }
