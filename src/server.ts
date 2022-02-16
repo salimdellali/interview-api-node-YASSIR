b27508ce29420b76e783dfecc990646374574b48
Salim DELLALI <dellali.salim@gmail.com>
2022-02-16 13:03:21 +0100

    merged app.ts content into server.ts file to make server.ts as a single entry point to the whole server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,24 @@
-import app from "./app";
+import fastify, {
+  FastifyServerOptions,
+  FastifyLoggerInstance,
+  FastifyInstance,
+} from "fastify";
+import { Server, IncomingMessage, ServerResponse } from "http";
+import router from "./routes/router";
 
 const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3000;
+
+const serverOptions: FastifyServerOptions<Server, FastifyLoggerInstance> = {
+  // Logger only for production
+  logger: !!(process.env.NODE_ENV !== "development"),
+};
+
+const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify(
+  serverOptions
+);
+
+// Middleware: Router
+app.register(router);
 
 app.listen(FASTIFY_PORT);
 
